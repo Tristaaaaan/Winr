6142d770e9b95bf986488f5acc16eeafd9947904
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-09-05 13:03:44 +0800

    feature: delete all records

--- lib/feature/settings/presentation/settings_screen.dart
+++ lib/feature/settings/presentation/settings_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:winr/common/components/buttons/loading_state_notifier.dart';
+import 'package:winr/common/components/buttons/regular_button.dart';
+import 'package:winr/common/components/snackbar/information_snackbar.dart';
+import 'package:winr/feature/history/presentation/providers/history_controller.dart';
 import 'package:winr/feature/settings/presentation/about_app.dart';
 import 'package:winr/feature/settings/presentation/feedback_and_suggestions.dart';
 
@@ -14,11 +18,75 @@ class SettingsScreen extends ConsumerWidget {
           slivers: [
             SliverList(
               delegate: SliverChildListDelegate([
-                SizedBox(height: 50),
-
+                SizedBox(height: 25),
                 BugSuggestionsReport(),
                 SizedBox(height: 50),
                 AboutApp(),
+                SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: RegularButton(
+                    suffixIcon: false,
+                    withIcon: false,
+                    text: "Delete All Records",
+                    onTap: () async {
+                      if (!context.mounted) return;
+
+                      final confirm = await showDialog<bool>(
+                        context: context,
+                        builder: (ctx) {
+                          return AlertDialog(
+                            title: const Text("Record Deletion"),
+                            content: const Text(
+                              "Are you sure you want to delete all the record? This action cannot be undone.",
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(ctx).pop(false),
+                                child: const Text("Cancel"),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.of(ctx).pop(true),
+                                style: TextButton.styleFrom(
+                                  foregroundColor: Theme.of(
+                                    context,
+                                  ).colorScheme.error,
+                                ),
+                                child: const Text("Delete"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+
+                      if (confirm != true) return;
+
+                      final isLoading = ref.read(
+                        regularButtonLoadingProvider.notifier,
+                      );
+                      isLoading.setLoading("deleteAllRecords", true);
+
+                      try {
+                        await ref
+                            .read(historyControllerProvider.notifier)
+                            .deleteAllRecords();
+
+                        if (!context.mounted) return;
+                        informationSnackBar(
+                          context,
+                          Icons.delete_outline,
+                          "Records has been deleted!",
+                        );
+                      } finally {
+                        isLoading.setLoading("deleteAllRecords", false);
+                      }
+                    },
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    textColor: Theme.of(context).colorScheme.surface,
+                    buttonKey: "deleteAllRecords",
+                    width: double.infinity,
+                  ),
+                ),
               ]),
             ),
           ],
