cbdd37182facbdf79a0c135e9d80c0f4cdacc61d
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-09-16 10:15:08 +0800

    chore: fix formatting for CI

---
+++ lib/core/appmodels/record.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'record.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$WinRateRecordsImpl _$$WinRateRecordsImplFromJson(Map<String, dynamic> json) =>
+    _$WinRateRecordsImpl(
+      id: (json['id'] as num?)?.toInt(),
+      timeAdded: (json['timeAdded'] as num).toInt(),
+      lastUpdated: (json['lastUpdated'] as num?)?.toInt(),
+      backgroundImage: json['backgroundImage'] as String?,
+      desiredWinRate: (json['desiredWinRate'] as num).toInt(),
+      name: json['name'] as String?,
+      currentNumberOfBattles: (json['currentNumberOfBattles'] as num).toInt(),
+      currentWinRate: (json['currentWinRate'] as num).toInt(),
+      progressiveWinRate: (json['progressiveWinRate'] as num?)?.toInt(),
+    );
+
+Map<String, dynamic> _$$WinRateRecordsImplToJson(
+  _$WinRateRecordsImpl instance,
+) => <String, dynamic>{
+  'id': instance.id,
+  'timeAdded': instance.timeAdded,
+  'lastUpdated': instance.lastUpdated,
+  'backgroundImage': instance.backgroundImage,
+  'desiredWinRate': instance.desiredWinRate,
+  'name': instance.name,
+  'currentNumberOfBattles': instance.currentNumberOfBattles,
+  'currentWinRate': instance.currentWinRate,
+  'progressiveWinRate': instance.progressiveWinRate,
+};
