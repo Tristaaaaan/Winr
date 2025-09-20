0477fc65d7c54e1b05ef8f155fd63957d938e0bf
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-09-20 16:05:38 +0800

    feature: scrolling up

--- lib/feature/settings/presentation/settings_screen.dart
+++ lib/feature/settings/presentation/settings_screen.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:winr/common/components/buttons/loading_state_notifier.dart';
-import 'package:winr/common/components/buttons/regular_button.dart';
-import 'package:winr/common/components/snackbar/information_snackbar.dart';
-import 'package:winr/feature/history/presentation/providers/history_controller.dart';
-import 'package:winr/feature/history/presentation/providers/result_provider.dart';
 import 'package:winr/feature/settings/presentation/widgets/about_app.dart';
 import 'package:winr/feature/settings/presentation/widgets/configuration.dart';
 import 'package:winr/feature/settings/presentation/widgets/feedback_and_suggestions.dart';
@@ -25,74 +20,7 @@ class SettingsScreen extends ConsumerWidget {
                 BugSuggestionsReport(),
                 SizedBox(height: 25),
                 AboutApp(),
-                SizedBox(height: 25),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: RegularButton(
-                    suffixIcon: false,
-                    withIcon: false,
-                    text: "Delete All Records",
-                    onTap: () async {
-                      if (!context.mounted) return;
-
-                      final confirm = await showDialog<bool>(
-                        context: context,
-                        builder: (ctx) {
-                          return AlertDialog(
-                            title: const Text("Record Deletion"),
-                            content: const Text(
-                              "Are you sure you want to delete all the record? This action cannot be undone.",
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.of(ctx).pop(false),
-                                child: const Text("Cancel"),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.of(ctx).pop(true),
-                                style: TextButton.styleFrom(
-                                  foregroundColor:
-                                      Theme.of(context).colorScheme.error,
-                                ),
-                                child: const Text("Delete"),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-
-                      if (confirm != true) return;
-
-                      final isLoading = ref.read(
-                        regularButtonLoadingProvider.notifier,
-                      );
-                      isLoading.setLoading("deleteAllRecords", true);
-
-                      try {
-                        await ref
-                            .read(historyControllerProvider.notifier)
-                            .deleteAllRecords();
-
-                        if (!context.mounted) return;
-                        informationSnackBar(
-                          context,
-                          Icons.delete_outline,
-                          "Records has been deleted!",
-                        );
-                      } finally {
-                        ref.invalidate(requiredWinsProvider);
-                        ref.invalidate(desiredWinRateProvider);
-                        ref.invalidate(numberOfBattlesProvider);
-                        ref.invalidate(winRateProvider);
-                        isLoading.setLoading("deleteAllRecords", false);
-                      }
-                    },
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    textColor: Theme.of(context).colorScheme.surface,
-                    buttonKey: "deleteAllRecords",
-                    width: double.infinity,
-                  ),
-                ),
+                SizedBox(height: 50),
               ]),
             ),
             SliverPadding(
--- lib/feature/settings/presentation/widgets/configuration.dart
+++ lib/feature/settings/presentation/widgets/configuration.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:winr/common/components/buttons/loading_state_notifier.dart';
 import 'package:winr/common/components/container/profile_settings.dart';
+import 'package:winr/common/components/snackbar/information_snackbar.dart';
 import 'package:winr/core/apptext/app_text.dart';
 import 'package:winr/core/appthemes/app_themes.dart';
+import 'package:winr/feature/history/presentation/providers/history_controller.dart';
+import 'package:winr/feature/history/presentation/providers/result_provider.dart';
 
 class AppConfiguration extends ConsumerWidget {
   const AppConfiguration({super.key});
 
   @override
-  Widget build(BuildContext contex, WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final themeNotifier = ref.watch(themeNotifierProvider.notifier);
 
     return Column(
@@ -33,6 +37,63 @@ class AppConfiguration extends ConsumerWidget {
                 await SharedPreferences.getInstance();
 
             prefs.setBool("theme", isLightMode);
+          },
+        ),
+        ProfileSettingsContainer(
+          withSwitch: false,
+          title: "Clear Records",
+          icon: Icons.delete_forever_outlined,
+          onTap: () async {
+            if (!context.mounted) return;
+
+            final confirm = await showDialog<bool>(
+              context: context,
+              builder: (ctx) {
+                return AlertDialog(
+                  title: const Text("Record Deletion"),
+                  content: const Text(
+                    "Are you sure you want to delete all the record? This action cannot be undone.",
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(ctx).pop(false),
+                      child: const Text("Cancel"),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.of(ctx).pop(true),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Theme.of(context).colorScheme.error,
+                      ),
+                      child: const Text("Delete"),
+                    ),
+                  ],
+                );
+              },
+            );
+
+            if (confirm != true) return;
+
+            final isLoading = ref.read(regularButtonLoadingProvider.notifier);
+            isLoading.setLoading("deleteAllRecords", true);
+
+            try {
+              await ref
+                  .read(historyControllerProvider.notifier)
+                  .deleteAllRecords();
+
+              if (!context.mounted) return;
+              informationSnackBar(
+                context,
+                Icons.delete_outline,
+                "Records has been deleted!",
+              );
+            } finally {
+              ref.invalidate(requiredWinsProvider);
+              ref.invalidate(desiredWinRateProvider);
+              ref.invalidate(numberOfBattlesProvider);
+              ref.invalidate(winRateProvider);
+              isLoading.setLoading("deleteAllRecords", false);
+            }
           },
         ),
       ],
