952e5c62bf7ce16c82542ed90c9b2cde935f51c8
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-09-24 09:43:40 +0800

    version: winr 1.0.13+13

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ if (localPropertiesFile.exists()) {
     }
 }
 
-val flutterVersionCode: Int = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 12
-val flutterVersionName: String = localProperties.getProperty("flutter.versionName") ?: "1.0.12"
+val flutterVersionCode: Int = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 13
+val flutterVersionName: String = localProperties.getProperty("flutter.versionName") ?: "1.0.13"
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
